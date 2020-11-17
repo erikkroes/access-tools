7ad1cbb4207dd70cfac85000ce67d96f2ab19f36
erik <erik.kroes@ing.com>
2020-11-17 13:08:18 +0100

    add filter to remove images without alt

--- content_script.js
+++ content_script.js
@@ -1,6 +1,20 @@
 const init = () => {
-  
-  if (browser.storage.sync.get("tabindex")) { 
+  console.log("Getting settings");
+
+  browser.storage.sync.get("tabindex").then(filterTabindex);
+  browser.storage.sync.get("no-alt").then(filterNoAlt);
+}
+
+try {
+  init();  
+} catch (error) {
+  console.error(`Access Tools error: ${error}`)
+}
+
+function filterTabindex(state) {
+  console.log(state);
+  if (state.tabindex == true) { 
+    console.log("tabindex set to " + state.tabindex);
     const allEl = document.querySelectorAll("body *[tabindex]"); 
 
     allEl.forEach(el => {
@@ -8,12 +22,16 @@ const init = () => {
         el.setAttribute('tabindex', 0);
       }
     });
+    console.log("tabindex ran!");
   }
-
 }
 
-try {
-  init();  
-} catch (error) {
-  console.error(`Access Tools error: ${error}`)
+function filterNoAlt(state) {
+  if (state.noalt == true) { 
+    const allEl = document.querySelectorAll("body img:not[alt], body img[alt='']"); 
+
+    allEl.forEach(el => {
+      el.setAttribute('hidden', true);
+    });
+  }  
 }
--- settings/script.js
+++ settings/script.js
@@ -1,29 +1,34 @@
-const button = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
+const filters = ['tabindex', 'no-alt'];
 
-button.addEventListener('click', (e) => {  
-  let el = e.target;
-  let state = el.getAttribute('aria-checked');
-  let isState = (state === 'true');
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', (e) => {  
+    const el = e.target;
+    const state = el.getAttribute('aria-checked');
+    const isState = (state === 'true');
+    
+    el.setAttribute('aria-checked', isState ? false : true);
   
-  el.setAttribute('aria-checked', isState ? false : true);
-
-  browser.storage.sync.set({
-    [el.id]: isState ? true : false
+    browser.storage.sync.set({
+      [el.id]: isState ? false : true
+    });
   });
-});
+}
 
 function restoreOptions() {
+    filters.forEach(filter => {
+      let getting = browser.storage.sync.get(filter);
+      getting.then(setCurrentChoice, onError);
 
-    function setCurrentChoice(result) {
-        document.querySelector("#tabindex").setAttribute('aria-checked', result.tabindex ? true : false);
-    }
-
-    function onError(error) {
-        console.log(`Error: ${error}`);
-    }
-
-    let getting = browser.storage.sync.get("tabindex");
-    getting.then(setCurrentChoice, onError);
+      function setCurrentChoice(result) {
+        document.querySelector("#"+filter).setAttribute('aria-checked', result[filter] ? true : false);
+        console.log("set " + filter + ": " + document.querySelector("#"+filter).getAttribute('aria-checked'));
+      }
+  
+      function onError(error) {
+          console.log(`Error: ${error}`);
+      }    
+    }) 
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
