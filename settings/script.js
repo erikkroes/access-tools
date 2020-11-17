17422cb06455c2891927fbe237367ece1f936494
erik <erik.kroes@ing.com>
2020-11-17 15:01:58 +0100

    Make it easy to add filters

--- settings/script.js
+++ settings/script.js
@@ -1,28 +1,35 @@
-const buttons = document.querySelectorAll('button');
-const filters = ['tabindex', 'no-alt'];
-
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', (e) => {  
-    const el = e.target;
-    const state = el.getAttribute('aria-checked');
-    const isState = (state === 'true');
-    
-    el.setAttribute('aria-checked', isState ? false : true);
-  
-    browser.storage.sync.set({
-      [el.id]: isState ? false : true
-    });
-  });
-}
-
-function restoreOptions() {
-    filters.forEach(filter => {
+function init() {
+    Object.entries(filters).forEach(([filter, value]) => {
       let getting = browser.storage.sync.get(filter);
-      getting.then(setCurrentChoice, onError);
+      getting.then(addFilter, onError);
 
-      function setCurrentChoice(result) {
-        document.querySelector("#"+filter).setAttribute('aria-checked', result[filter] ? true : false);
-        console.log("set " + filter + ": " + document.querySelector("#"+filter).getAttribute('aria-checked'));
+      function addFilter(result) {
+        // add the HTML to the settings page
+        const list = document.querySelector('.filters__list');
+        const output = `<li class="filter">
+        <label for="${filter}" class="filter__label"> 
+          <h3 class="filter__title">${value.name}</h3>
+          <button type="button" id="${filter}" role="switch" aria-checked="${result[filter] ? "true" : "false"}">
+            <span>on</span><span>off</span>  
+          </button>
+        </label>
+        <p>${value.desc}</p>
+      </li>`;      
+        list.insertAdjacentHTML("beforeend", output);
+
+        // Set up the switch on the settings page
+        const button = document.querySelector("#"+filter);              
+        button.addEventListener('click', (e) => {  
+          const el = e.target;
+          const state = el.getAttribute('aria-checked');
+          const isState = (state === 'true');
+          
+          el.setAttribute('aria-checked', isState ? false : true);
+          
+          browser.storage.sync.set({
+            [el.id]: isState ? false : true
+          });
+        });        
       }
   
       function onError(error) {
@@ -31,5 +38,5 @@ function restoreOptions() {
     }) 
 }
 
-document.addEventListener("DOMContentLoaded", restoreOptions);
+document.addEventListener("DOMContentLoaded", init);
   
