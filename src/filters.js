d7e40911d1d647c568804ce056584cab5482c773
erik <erik.kroes@ing.com>
2021-06-11 22:37:42 +0200

    restructure

--- src/filters.js
+++ src/filters.js
@@ -3,8 +3,8 @@ const filters = {
     name: "No positive tabindex",
     desc: "Positive tabindex gets set to 0 instead.",
     func: function() {
-      const allEl = document.querySelectorAll("body *[tabindex]"); 
-  
+      const allEl = document.querySelectorAll("body *[tabindex]");
+
       allEl.forEach(el => {
         if(el.getAttribute('tabindex') > 0) {
           el.setAttribute('tabindex', 0);
@@ -16,7 +16,7 @@ const filters = {
     name: "Hide decorative images",
     desc: "Hide images that have an empty alt or no alt at all",
     func: function(){
-      const allEl = document.querySelectorAll("body img:not[alt], body img[alt='']"); 
+      const allEl = document.querySelectorAll("body img:not[alt], body img[alt='']");
 
       allEl.forEach(el => {
         el.setAttribute('hidden', true);
